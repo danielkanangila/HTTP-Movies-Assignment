6ee9b8ef894c749c0133dd37aa9b8fbd4bf22f55
Daniel Kanangila <daniel.kanangila@gmail.com>
2020-03-20 01:57:27 -0400

    Moved getMovieList to MovieList component

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,6 @@ import { Route } from "react-router-dom";
 import SavedList from "./components/Movies/SavedList";
 import MovieList from "./components/Movies/MovieList";
 import Movie from "./components/Movies/Movie";
-import axios from 'axios';
 import NavBar from "./components/Navbar/NavBar";
 import Footer from "./components/Footer";
 import Form from "./components/Movies/Form";
@@ -11,29 +10,17 @@ import DeleteMovie from "./components/Movies/DeleteMovie";
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
-  const [movieList, setMovieList] = useState([]);
-
-  const getMovieList = () => {
-    axios
-      .get("http://localhost:5000/api/movies")
-      .then(res => setMovieList(res.data))
-      .catch(err => console.log(err.response));
-  };
 
   const addToSavedList = movie => {
     setSavedList([...savedList, movie]);
   };
-
-  useEffect(() => {
-    getMovieList();
-  }, []);
 
   return (
     <div className="app">
       <NavBar />
       <div className="container">
         <SavedList list={savedList} />
-        <Route exact path="/" render={() => <MovieList movies={movieList} /> }/>
+        <Route exact path="/" render={() => <MovieList /> }/>
         <Route path="/movies/:id" render={() => <Movie addToSavedList={addToSavedList} />} />
         <Route path="/add-movie" render={props => <Form {...props} />} />
         <Route path="/update-movie/:id" render={props => <Form  {...props}/>} />
--- client/src/components/Movies/MovieList.js
+++ client/src/components/Movies/MovieList.js
@@ -1,8 +1,22 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import axios from 'axios';
 import { Link } from "react-router-dom";
 import MovieCard from "./MovieCard";
 
-const MovieList = ({ movies }) => {
+const MovieList = () => {
+  const [movies, setMovieList] = useState([]);
+
+  const getMovieList = () => {
+    axios
+      .get("http://localhost:5000/api/movies")
+      .then(res => setMovieList(res.data))
+      .catch(err => console.log(err.response));
+  };
+
+  useEffect(() => {
+    getMovieList();
+  }, []);
+
   return (
     <div className="movie-list">
       {
