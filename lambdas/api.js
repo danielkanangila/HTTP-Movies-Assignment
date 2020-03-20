9f63b5923137e8382d946789b45b1e007230e62d
Daniel Kanangila <daniel.kanangila@gmail.com>
2020-03-20 15:44:01 -0400

    Updated server files

--- lambdas/api.js
+++ lambdas/api.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const CORS = require("cors");
-const apiRouter = require("./api/apiRouter");
+const apiRouter = require("../api/apiRouter");
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(CORS());
-app.use(apiRouter);
+app.use("/http-movies", apiRouter);
 
-app.listen(5000, () => {
-  console.log("Server listening on port 5000");
-});
+module.exports = apiRouter;
