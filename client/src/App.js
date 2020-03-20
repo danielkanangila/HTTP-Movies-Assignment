0e1ddd597a95df094ccad8101bd6f09d970559c8
Daniel Kanangila <daniel.kanangila@gmail.com>
2020-03-20 17:41:01 -0400

    removed useEffect in App comp

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Route } from "react-router-dom";
 import SavedList from "./components/Movies/SavedList";
 import MovieList from "./components/Movies/MovieList";
